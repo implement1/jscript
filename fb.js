ff1b31d568bf7b80a239cc7d087e49031615c8b4
chitou <chitouatanda@gmail.com>
2022-05-19 21:05:40 -0400

    fizzbuzz

---
+++ fb.js
@@ -0,0 +1,10 @@
+function fb(num) {
+    for (var i = 1; i <= num; i++) {
+        if (i % 15 === 0) console.log ('FizzBuzz');
+        else if (i % 5 === 0) console.log ('Fizz');
+        else if (i % 3 === 0) console.log ('Buzz');
+        else console.log(i);
+    }
+}
+
+fb(15);

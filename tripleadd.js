c734aa9b60dae9ea924964a0ed0cc86a8fc24a92
chitou <chitouatanda@gmail.com>
2022-05-22 22:55:54 -0400

    tripleadd

---
+++ tripleadd.js
@@ -0,0 +1,10 @@
+function tripleadd (num1) {
+    return function (num2) {
+        return function (num3) {
+            return num1 + num2 + num3;
+        }
+    }
+
+}
+
+console.log (tripleadd (10)(10)(30));
